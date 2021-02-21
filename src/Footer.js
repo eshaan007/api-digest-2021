91e8588087afb29cbf1010530f8e820f913d8f77
eshaan007 <eshaany2k@gmail.com>
2021-02-21 21:21:55 +0530

    :rocket:

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 const Footer = () => {
     return (
         <div className="footer">
-            <h4>Made with ❤️ by Tekk Force</h4>
+            <h4>Made with ❤️ by Tekk-Force</h4>
         </div>
     );
 };
